451a0cff29a7c7bb3ea3f8a16552c9c207c382a5
erubtsov <39719687+erubtsov@users.noreply.github.com>
2024-03-25 11:22:23 -0600

    Update main_app.py
    fix layout and popups. Updated for most developed version of app

--- client/main_app.py
+++ client/main_app.py
@@ -1,14 +1,15 @@
-# main_app.py
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from frontend_components import PasswordPopup, ErrorPopup, DirectorySelectPopup, QueryResultPopup, ViewUploadDetailsPopup, ButtonDisplay
+from frontend_components import PasswordPopup, ErrorPopup, DirectorySelectPopup, QueryResultPopup, ViewUploadDetailsPopup
 from backend_communication import BackendCommunication
+from kivy.utils import get_color_from_hex
 
 class PostgreSQLApp(App):
     def __init__(self, **kwargs):
         super(PostgreSQLApp, self).__init__(**kwargs)
         self.backend_communication = None
+        self.query_result_popup = None
         self.password_popup = None
         self.password = None
         self.selected_directories = {
@@ -17,8 +18,9 @@ class PostgreSQLApp(App):
             "Live Print Data": "",
             "Parts Quality": ""
         }
-        self.on_stop = None  # Define on_stop attribute here
         self.query_database = None
+        self.uploading_data = False  # Track if data is being uploaded
+        self.buttons = {}  # Dictionary to store buttons
         
     def build(self):
         self.layout = BoxLayout(orientation='vertical')
@@ -29,8 +31,12 @@ class PostgreSQLApp(App):
         self.password_popup = PasswordPopup(callback=self.submit_password)
         self.password_popup.open()
 
+    def show_query_result_popup(self, query_result):
+        self.query_result_popup = QueryResultPopup(query_result=query_result)
+        self.query_result_popup.open()
+
     def submit_password(self, password):
-        self.backend_communication = BackendCommunication()  # Instantiate the BackendCommunication object
+        self.backend_communication = BackendCommunication(query_result_popup_callback=self.show_query_result_popup)
         if self.backend_communication.verify_password(password):
             print("Password correct. You can now upload data.")
             self.query_database = self.backend_communication.query_database
@@ -38,14 +44,15 @@ class PostgreSQLApp(App):
         else:
             print("Incorrect password. Please try again.")
             self.show_password_error()
-
+    
     def upload_data(self):
-        if self.password is None:
-            print("Please submit password first.")
-            return
-
         if any(self.selected_directories.values()):
-            self.backend_communication.upload_data(self.selected_directories, self.password)
+            if not self.uploading_data:  # Check if data is not currently being uploaded
+                self.uploading_data = True  # Set uploading flag
+                self.backend_communication.upload_data(self.selected_directories)  # Pass selected directories only
+                # Optionally, you can trigger an event/callback to enable the upload button
+            else:
+                print("Data upload in progress. Please wait.")
         else:
             print("Please select at least one directory before uploading data.")
     
@@ -55,29 +62,67 @@ class PostgreSQLApp(App):
 
     def open_directory_selector(self, purpose):
         popup = DirectorySelectPopup(callback=self.select_directory_callback, purpose=purpose)
-        popup.bind(on_dismiss=lambda instance: self.update_button_text(purpose, instance.text_input.text))
         popup.open()
 
-    def select_directory_callback(self, directory_path, purpose):
-        self.selected_directories[purpose] = directory_path
-        print(f"Selected {purpose} directory:", directory_path)
+    def select_directory_callback(self, data):
+            purpose = data["purpose"]
+            selected_dir = data["directory_path"]
+            self.directory_selected(selected_dir, purpose)
+            print("Selected Directories:", self.selected_directories)  # Print selected directories
 
-    def update_button_text(self, purpose, directory_path):
-        for widget in self.layout.children:
-            if isinstance(widget, Button) and f"Select {purpose} Directory" in widget.text:
-                widget.text = f"Selected Path: {directory_path}" if directory_path else f"Select {purpose} Directory"
+    def directory_selected(self, selected_dir, purpose):
+            if selected_dir:
+                self.selected_directories[purpose] = selected_dir  # Remove [0] to get the full directory path
+                self.update_button_text(f"Select {purpose} Directory", selected_dir, purpose)  # Update button text
+                print(f"Selected {purpose} Directory:", selected_dir)  # Print selected directory
 
+    def update_button_text(self, button_text, directory_path, purpose):
+        button = self.buttons.get(button_text)
+        if button:
+            button.text = f"Selected Path: {directory_path}" if directory_path else f"Select {purpose} Directory"
+    
     def show_buttons(self):
         self.layout.clear_widgets()
 
-        button_texts = [f"Select {purpose} Directory" for purpose in self.selected_directories.keys()] + ["Query Database", "View Upload Details", "Upload Data"]
-        button_callbacks = [lambda instance, purpose=purpose: self.open_directory_selector(purpose) for purpose in self.selected_directories.keys()] + [self.query_database, self.view_upload_details, lambda instance: self.upload_data()]
-        buttons_layout = ButtonDisplay(
-                button_texts=button_texts, 
-                button_callbacks=button_callbacks, 
-                backend_callback=self.backend_communication.connect_to_backend
-                )
-        self.layout.add_widget(buttons_layout)
+        # Create a GridLayout for the top rows
+        top_rows_layout = BoxLayout(orientation='vertical')
+
+        button_texts = [f"Select {purpose} Directory" for purpose in self.selected_directories.keys()]
+        button_callbacks = [lambda instance, purpose=purpose: self.open_directory_selector(purpose) for purpose in self.selected_directories.keys()]
+        purposes = list(self.selected_directories.keys())  # Get the list of purposes
+        purposes.extend([None] * 3)  # Add None for the additional buttons
+
+        # Add the "Select Directory" buttons to the top rows layout
+        for text, callback in zip(button_texts, button_callbacks):
+            button = Button(text=text)  # Create button instance
+            button.bind(on_press=callback)  # Bind callback function
+            button.disabled_color = get_color_from_hex("#808080")  # Set the disabled color
+            self.buttons[text] = button  # Store the button in the dictionary
+            top_rows_layout.add_widget(button)  # Add button to top rows layout
+
+        # Add the top rows layout to the main layout
+        self.layout.add_widget(top_rows_layout)
+
+        # Create a BoxLayout for the bottom row
+        bottom_row_layout = BoxLayout(orientation='horizontal')
+
+        # Add the "Query Database", "View Upload Details", and "Upload Data" buttons to the bottom row layout
+        button_texts = ["Query Database", "View Upload Details", "Upload Data"]
+        button_callbacks = [
+            lambda instance, callback=self.query_database: callback(),
+            self.view_upload_details,
+            lambda instance: self.upload_data()
+        ]
+
+        for text, callback in zip(button_texts, button_callbacks):
+            button = Button(text=text)  # Create button instance
+            button.bind(on_press=lambda instance, callback=callback: callback(instance))
+            button.disabled_color = get_color_from_hex("#808080")  # Set the disabled color
+            self.buttons[text] = button  # Store the button in the dictionary
+            bottom_row_layout.add_widget(button)  # Add button to bottom row layout
+
+        # Add the bottom row layout to the main layout
+        self.layout.add_widget(bottom_row_layout)
 
     def show_password_error(self):
         error_popup = ErrorPopup(
